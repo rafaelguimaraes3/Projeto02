7f28fbcd64bd6028828651f08be65f1ac1a0bca7
rafaelguimaraes3 <rafael.guimaraes.tavares@gmail.com>
2021-05-17 00:29:26 -0300

    segunda tela

---
+++ app/src/main/java/rafael/guimaraes/projeto02/ListaProdutos.java
@@ -0,0 +1,28 @@
+package rafael.guimaraes.projeto02;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.View;
+
+public class ListaProdutos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_produtos);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    public void telaInicial (View view){
+
+    }
+
+}
--- app/src/main/java/rafael/guimaraes/projeto02/MainActivity.java
+++ app/src/main/java/rafael/guimaraes/projeto02/MainActivity.java
@@ -3,12 +3,30 @@ package rafael.guimaraes.projeto02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botao_lista_produto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inicializaComponentes();
+        botao_lista_produto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    private void inicializaComponentes(){
+        botao_lista_produto = findViewById(R.id.botao_lista_produto);
     }
 }
