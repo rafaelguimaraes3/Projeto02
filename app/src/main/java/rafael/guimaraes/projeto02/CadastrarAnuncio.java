c7da420cf6032ede9a284a876760044c37113184
rafaelguimaraes3 <rafael.guimaraes.tavares@gmail.com>
2021-05-19 00:32:42 -0300

    Recurso da camera

--- app/src/main/java/rafael/guimaraes/projeto02/CadastrarAnuncio.java
+++ app/src/main/java/rafael/guimaraes/projeto02/CadastrarAnuncio.java
@@ -2,13 +2,47 @@ package rafael.guimaraes.projeto02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class CadastrarAnuncio extends AppCompatActivity {
+    //permisões camera
+    private String[] permissoesNecessarias = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+
+    private ImageButton imageButtonCamera, imageButtonGaleria;
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        //Permissao.validarPermissoes(permissoesNecessarias, activity:this, requestCode:1);
+
+     imageButtonCamera = findViewById(R.id.imageButtonCamera);
+     imageButtonGaleria = findViewById(R.id.imageButtonGaleria);
+
+     imageButtonCamera.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+             if( i.resolveActivity(getPackageManager())!=null){
+                 startActivityForResult(i, SELECAO_CAMERA);
+             }
+
+         }
+     });
     }
+
 }
